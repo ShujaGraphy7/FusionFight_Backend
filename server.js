56c0bca92ab65f32bdb4bb4b84a061532b5921f3
ShujaGraphy7 <shujaabrar7@gmail.com>
2024-02-18 21:09:01 +0500

    update

--- server.js
+++ server.js
@@ -33,5 +33,5 @@ app.use('/users', userRoutes);
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
