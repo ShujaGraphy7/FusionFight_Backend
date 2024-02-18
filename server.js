bed705d8bb0036a4e109a80d152ed382df0d43f0
ShujaGraphy7 <shujaabrar7@gmail.com>
2024-02-18 21:26:37 +0500

    update backend

--- server.js
+++ server.js
@@ -21,13 +21,6 @@ app.use(bodyParser.json());
 // Connect to MongoDB
 connectDB();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
 // Routes
 app.use('/users', userRoutes);
 
