216e885cf3644650d07e1fe7b46cd965a1d52cca
ShujaGraphy7 <shujaabrar7@gmail.com>
2024-03-14 23:35:42 +0500

    update

--- controllers/airdropController.js
+++ controllers/airdropController.js
@@ -1,16 +1,31 @@
 const Distribute = require("../SkaleAirdropHelper/distribute");
 const Balance = require("../SkaleAirdropHelper/balance");
+const { JsonRpcProvider } = require("ethers");
+const { ethers } = require("ethers");
+const { RPC_URL } = require("../SkaleAirdropHelper/config");
 
 exports.claimAirdrop = async (req, res) => {
-  console.log(req)
   try {
     const { walletAddress } = req.params;
+    const provider = new JsonRpcProvider(RPC_URL);
+    const balance = await provider.getBalance(walletAddress);
 
-    const distributeResult = await Distribute({
-      address: walletAddress,
-    });
+    if (balance < 100000000000000) {
+      // If balance is less than 0.00001 ethers, distribute airdrop
+      const distributeResult = await Distribute({
+        address: walletAddress,
+      });
 
-    res.status(200).json({ distributeResult });
+      res.status(200).json({
+        message: "Airdrop distributed successfully",
+        distributeResult,
+      });
+    } else {
+      // If balance is greater than 0.00001 ethers, do not distribute airdrop
+      res.status(200).json({
+        message: "Balance is sufficient, no need for airdrop distribution",
+      });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
