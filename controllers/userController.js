924c3aced9720ccd2707fd65844ecf4c73dd882e
ShujaGraphy7 <shujaabrar7@gmail.com>
2024-03-14 20:22:27 +0500

    update backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,11 @@
-// controllers/userController.js
-
 const User = require("../models/user");
+const { isAuthenticated, hasPlayedGame } = require("../middleware/middlewares");
 
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.find(
       {},
-      "username walletAddress wonMatches lostMatches totalMatches totalPoints lastTokenUpdate"
+      "username walletAddress wonMatches lostMatches totalMatches totalPoints lastTokenUpdate referralCode"
     );
     res.status(200).json({ users });
   } catch (error) {
@@ -17,7 +16,7 @@ exports.getAllUsers = async (req, res) => {
 
 exports.registerUser = async (req, res) => {
   try {
-    const { username, walletAddress } = req.body;
+    const { username, walletAddress, referralCode } = req.body;
 
     // Check if a user with the same wallet address already exists
     const existingUser = await User.findOne({ walletAddress });
@@ -31,14 +30,42 @@ exports.registerUser = async (req, res) => {
 
     const lowercaseWalletAddress = walletAddress.toLowerCase();
 
-    // Create a new user
+    // Check if a referral code is provided and handle the referral system
+    if (referralCode) {
+      const referrer = await User.findOne({ referralCode });
+
+      if (referrer) {
+        // If referrer is found, create a new user with the referral code
+        const newUser = new User({
+          username,
+          walletAddress: lowercaseWalletAddress,
+          totalPoints: 50 + 25, // Initial points (50) + referral points (20)
+        });
+
+        // Save new user to the database
+        await newUser.save();
+
+        // Give 25 points to the referrer
+        referrer.totalPoints += 50;
+
+        // Save the changes to the referrer
+        await referrer.save();
+
+        return res.status(201).json({ message: "User registered successfully" });
+      } else {
+        // If referrer is not found, return an error
+        return res.status(400).json({ message: "Invalid referral code" });
+      }
+    }
+
+    // If no referral code is provided, create a new user without referral points
     const newUser = new User({
       username,
       walletAddress: lowercaseWalletAddress,
-      totalPoints:50
+      totalPoints: 50,
     });
 
-    // Save user to the database
+    // Save new user to the database
     await newUser.save();
 
     res.status(201).json({ message: "User registered successfully" });
@@ -48,7 +75,7 @@ exports.registerUser = async (req, res) => {
   }
 };
 
-exports.updateWonMatch = async (req, res) => {
+exports.updateWonMatch = [isAuthenticated, async (req, res) => {
   try {
     const { walletAddress } = req.params;
     const lowercaseWalletAddress = walletAddress.toLowerCase();
@@ -56,7 +83,7 @@ exports.updateWonMatch = async (req, res) => {
     // Increment the wonMatches and totalMatches counters for the specified user
     await User.findOneAndUpdate(
       { walletAddress: lowercaseWalletAddress },
-      { $inc: { wonMatches: 1, totalMatches: 1, totalPoints: 2 } }
+      { $inc: { wonMatches: 1, totalMatches: 1, totalPoints: 5 } }
     );
 
     res.status(200).json({ message: "Won match updated successfully" });
@@ -64,9 +91,9 @@ exports.updateWonMatch = async (req, res) => {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
   }
-};
+}];
 
-exports.updateLostMatch = async (req, res) => {
+exports.updateLostMatch = [isAuthenticated, async (req, res) => {
   try {
     const { walletAddress } = req.params;
     const lowercaseWalletAddress = walletAddress.toLowerCase();
@@ -82,7 +109,7 @@ exports.updateLostMatch = async (req, res) => {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
   }
-};
+}];
 
 exports.getUserDetails = async (req, res) => {
   try {
@@ -106,27 +133,14 @@ exports.getUserDetails = async (req, res) => {
   }
 };
 
-exports.updateUserPoints = async (req, res) => {
+exports.updateUserPoints = [isAuthenticated, async (req, res) => {
   try {
-    const { walletAddress } = req.params;
+    const { user } = req;
 
     // Get the current timestamp
     const currentTime = Date.now();
 
-    // Convert the wallet address to lowercase for case-insensitive search
-    const lowercaseWalletAddress = walletAddress.toLowerCase();
-
-    // Find the current user using a case-insensitive search
-    const currentUser = await User.findOne(
-      { walletAddress: lowercaseWalletAddress },
-      "walletAddress totalPoints lastTokenUpdate"
-    );
-
-    if (!currentUser) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    const { lastTokenUpdate } = currentUser;
+    const { lastTokenUpdate } = user;
 
     // Calculate time difference in hours
     const timeDifferenceHours =
@@ -135,7 +149,7 @@ exports.updateUserPoints = async (req, res) => {
     // Update total points only if 24 hours have passed since the last match
     if (timeDifferenceHours >= 24) {
       await User.findOneAndUpdate(
-        { walletAddress: lowercaseWalletAddress },
+        { walletAddress: user.walletAddress },
         { $inc: { totalPoints: 5 }, lastTokenUpdate: currentTime }
       );
 
@@ -149,53 +163,51 @@ exports.updateUserPoints = async (req, res) => {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
   }
-};
+}];
 
-exports.isEligible = async (req, res) => {
-  try {
-    const { walletAddress } = req.params;
-    const lowercaseWalletAddress = walletAddress.toLowerCase();
-
-    // Find the user by wallet address
-    const user = await User.findOne({ walletAddress: lowercaseWalletAddress }, "walletAddress lastTokenUpdate");
-
-    if (!user) {
-      return res.status(404).json({ message: "User has not Started a battle", data: { is_ok: false } });
-    }
+exports.isEligible = [isAuthenticated, async (req, res) => {
+  try {    
+    const { user } = req;
 
     // Get the current timestamp
     const { lastTokenUpdate } = user;
 
     // Calculate the timestamp representing 24 hours ago
-    const twentyFourHoursAgo = Date.now() - (24 * 60 * 60 * 1000);
+    const twentyFourHoursAgo = Date.now() - 24 * 60 * 60 * 1000;
 
     const isOk = lastTokenUpdate > twentyFourHoursAgo;
 
     res.status(200).json({ data: { is_ok: isOk } });
-
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error', error: error.message });
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
-};
+}];
 
-exports.isRegisteredUser = async (req, res) => {
+exports.isRegisteredUser = [isAuthenticated, async (req, res) => {
   try {
-    const { walletAddress } = req.params;
-    const lowercaseWalletAddress = walletAddress.toLowerCase();
-
-    // Find the user by wallet address
-    const user = await User.findOne({ walletAddress:lowercaseWalletAddress });
-
-    if (!user) {
-      return res.status(404).json({data: { is_ok: false }  });
-    }
-
-
     res.status(200).json({ data: { is_ok: true } });
-    
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error', error: error.message });
+    res.status(500).json({ message: "Internal server error" });
   }
-};
+}];
+
+exports.bugBounty = [isAuthenticated, hasPlayedGame, async (req, res) => {
+  try {
+    const { user } = req;
+
+    // Assuming bug bounty reward is 5 points
+    user.totalPoints += 5;
+
+    // Save the changes
+    await user.save();
+
+    res.status(200).json({ message: "Bug bounty points added successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}];
