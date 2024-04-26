b8cc18bbf6c3bd93074ed213f7b9beebf7ccaa14
ShujaGraphy7 <shujaabrar7@gmail.com>
2024-04-26 22:16:36 +0500

    update

--- routes/airdropRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const airdropController = require('../controllers/airdropController');
-
-router.get('/claim/:walletAddress', airdropController.claimAirdrop);
-router.get('/balance', airdropController.getBalance);
-router.get('/balanceOf/:walletAddress', airdropController.getBalanceOf);
-
-module.exports = router;
